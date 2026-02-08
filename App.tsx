a0964b1a6e5ff82036bb0886f14a6b13e521001a
nidkhan142-glitch <nidkhan142@gmail.com>
2026-02-09 00:32:47 +0500

    Fix Header import path in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Header } from './Header';
+import Header from './Header';
+import './App.css';
 
-const App = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Other components and code */}
+    </div>
+  );
+}
 
 export default App;
